6f47416cab1f22577f24972ec4f23df68ff0deed
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-22 15:14:10 +0300

    task 4 resolved

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Введите 3 числа: ");
+string numberA = Console.ReadLine();
+string numberB = Console.ReadLine();
+string numberC = Console.ReadLine();
+
+int convertedNumberA = int.Parse(numberA);
+int convertedNumberB = int.Parse(numberB);
+int convertedNumberC = int.Parse(numberC);
+
+int max = convertedNumberA;
+
+if (convertedNumberB > max)
+{
+    max = convertedNumberB;
+}
+
+if (convertedNumberC > max)
+{
+    max = convertedNumberC;
+}
+
+Console.Write("max = ");
+Console.Write(max);

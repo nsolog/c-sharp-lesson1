cb422fe9739354f31f35a12debbf6b3a12399833
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-22 14:29:40 +0300

    Task resolved

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+string a = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string b = Console.ReadLine();
+
+int convertedA = int.Parse(a);
+int convertedB = int.Parse(b);
+
+if (convertedA > convertedB)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}

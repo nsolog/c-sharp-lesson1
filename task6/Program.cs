343a3297d17fdf9c3d5ccb486e3c145e512e5ad3
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-22 15:25:49 +0300

    Task 6 resolved

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+float number = Console.Read();
+
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число не чётное");
+}
